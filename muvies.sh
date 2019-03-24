ea11455b32143ead74a891c868e57b3e5ae71b52
Cipherbunker <41933480+Cipherbunker@users.noreply.github.com>
2019-03-24 14:35:35 -0700

    Update muvies.sh

--- muvies.sh
+++ muvies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # upload&download script
 
-mp4m.sh && ./ziperm.sh && curl -u technoshaman:Skurupey1! -F fileupload1=@muvies.zip -F press="Upload files" http://http://98.145.138.85:76/Videos/Music && ./ mp4d.sh
+mp4m.sh && ./ziperm.sh && curl -u username:password -F fileupload1=@muvies.zip -F press="Upload files" http://http://ip/Videos/Music && ./ mp4d.sh
